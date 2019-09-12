d5c4ac473e2614cb34fac49db797f99f497ca682
ECWireless <Elliott@valtechcreative.com>
2019-09-12 12:08:39 -0400

    Basic layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import './App.scss';
 
-function App() {
-	return (
-		<div className="App">
-			Grant Taxidermy
-		</div>
-  );
+export default class App extends Component {
+	render() {
+		return (
+			<div className="container">
+				<header className="header">Header</header>
+				<div className="banner">Home Banner</div>
+				<div className="services">Services</div>
+				<div className="about">About</div>
+				<div className="photos">Photos</div>
+				<div className="testimonials">Testimonials</div>
+				<div className="contact">Contact</div>
+				<footer className="footer">Footer</footer>
+			</div>
+		)
+	}
 }
-
-export default App;
