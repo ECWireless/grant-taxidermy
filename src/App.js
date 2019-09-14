63af5d22d15b650dabdf2e36690f25673bbc845e
ECWireless <Elliott@valtechcreative.com>
2019-09-14 11:16:53 -0400

    Contact complete

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ import About from './containers/About';
 import Photos from './containers/Photos';
 import Testimonials from './containers/Testimonials';
 
+// Photos
+import Logo from './assets/banner/harry-grant-taxidermy-logo-cutout-antlers-2@2x.png'
+
 export default class App extends Component {
 
 	render() {
@@ -30,11 +33,44 @@ export default class App extends Component {
 
 					<div className="contact__background">
 						<div className="contact__container">
-							<img src="" alt="contact-logo" className="contact__logo" />
-							<div className="contact__info">Contact Info</div>
-							<div className="contact__form">Contact Form</div>
+							<img src={Logo} alt="contact-logo" className="contact__logo" />
+							<div className="contact__info">
+								<div className="contact__info-paragraph-1">5392 Hardt Road</div>
+								<div className="contact__info-paragraph-1">Gibsonia PA, 15044</div>
+								<br/>
+								<br/>
+								<div className="contact__info-paragraph-1">info@harrygranttaxidermy.com</div>
+								<br/>
+								<br/>
+								<div className="contact__info-paragraph-2">412-400-7308</div>
+								<br/>
+								<br/>
+								<br/>
+								<br/>
+								<div className="contact__info-paragraph-3">Hours:</div>
+								<br/>
+								<div className="contact__info-paragraph-4">Mon - Fri: 9am - 7pm</div>
+								<div className="contact__info-paragraph-4">Saturday: 9am - 10am</div>
+								<div className="contact__info-paragraph-4">Sunday: 9am - 6pm</div>
+							</div>
+							<div className="contact__form">
+								<label htmlFor="name">Name</label>
+								<input type="text" id="name" />
+								<br/>
+								<label htmlFor="email">Email</label>
+								<input type="email" id="email" />
+								<br/>
+								<label htmlFor="subject">Subject</label>
+								<input type="text" id="subject" />
+								<br/>
+								<label htmlFor="message">Message</label>
+								<textarea name="message" id="message" cols="30" rows="10"></textarea>
+								<br/>
+								<button className="contact__button">Submit</button>
+							</div>
 						</div>
 					</div>
+					<div className="contact__background-block" />
 				</div>
 				<footer className="footer">Footer</footer>
 			</div>
