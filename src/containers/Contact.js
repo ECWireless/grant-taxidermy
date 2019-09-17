d9becc187d612276ba9cad8d061ab97975417d0d
ECWireless <Elliott@valtechcreative.com>
2019-09-17 11:30:01 -0400

    Form change

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -26,6 +26,30 @@ export default function Contact() {
                     <div className="contact__info-paragraph-4">Saturday: 9am - 10am</div>
                     <div className="contact__info-paragraph-4">Sunday: 9am - 6pm</div>
                 </div>
+
+                <form action="#" className="form">
+                    <div className="form__group">
+                            <input type="text" className="form__input" placeholder="Full Name" id="name" required />
+                            <label htmlFor="name" className="form__label">Name</label>
+                    </div>
+
+                    <div className="form__group">
+                            <input type="email" className="form__input" placeholder="Email Address" id="email" required />
+                            <label htmlFor="email" className="form__label">Email</label>
+                    </div>
+
+                    <div className="form__group">
+                            <input type="tel" className="form__input" placeholder="Phone Number" id="phone" required />
+                            <label htmlFor="phone" className="form__label">Subject</label>
+                    </div>
+
+                    <div className="form__group">
+                            <textarea className="form__input" placeholder="Your Message" id="message" required />
+                            <label htmlFor="message" className="form__label">Message</label>
+                    </div>
+                    <button className="contact__button">Submit</button>
+                </form>
+{/*                 
                 <div className="contact__form">
                     <label htmlFor="name">Name</label>
                     <input type="text" id="name" />
@@ -39,8 +63,8 @@ export default function Contact() {
                     <label htmlFor="message">Message</label>
                     <textarea name="message" id="message" cols="30" rows="10"></textarea>
                     <br/>
-                    <button className="contact__button">Submit</button>
-                </div>
+                    
+                </div> */}
             <div className="contact__background-block" />
         </div>
     )
